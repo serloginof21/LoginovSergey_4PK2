b44cabb0ecc8344461391627fbb3e22d1187f400
Sergey Loginov <sergefa29@mail.ru>
2023-11-16 19:45:07 +0500

    PZ_25

---
+++ PZ_25/App.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace PZ_25
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+            this.Resuming += OnResuming;
+            this.EnteredBackground += OnEnteredBackground;
+            this.LeavingBackground += OnLeavingBackground;
+        }
+
+        private async void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            var dialog = new Windows.UI.Popups.MessageDialog("Приложение приостанавливается");
+            await dialog.ShowAsync();
+            deferral.Complete();
+        }
+
+        private async void OnResuming(object sender, object e)
+        {
+            var dialog = new Windows.UI.Popups.MessageDialog("Приложение возобновляется");
+            await dialog.ShowAsync();
+        }
+
+        private async void OnEnteredBackground(object sender, EnteredBackgroundEventArgs e)
+        {
+            var dialog = new Windows.UI.Popups.MessageDialog("Приложение в фоновом режиме");
+            await dialog.ShowAsync();
+        }
+
+        private async void OnLeavingBackground(object sender, LeavingBackgroundEventArgs e)
+        {
+            var dialog = new Windows.UI.Popups.MessageDialog("Приложение возвращается из фонового режима");
+            await dialog.ShowAsync();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Загрузить состояние из ранее приостановленного приложения
+                }
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // Если стек навигации не восстанавливается для перехода к первой странице,
+                    // настройка новой страницы путем передачи необходимой информации в качестве параметра
+                    // навигации
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Обеспечение активности текущего окна
+                Window.Current.Activate();
+            }
+        }
+        /// <summary>
+        /// Вызывается в случае сбоя навигации на определенную страницу
+        /// </summary>
+        /// <param name="sender">Фрейм, для которого произошел сбой навигации</param>
+        /// <param name="e">Сведения о сбое навигации</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+    }
+}
